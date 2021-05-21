c540e51cc63fc4486b60b5fec461db76cc1703de
Nick Musey <nmusey@gmail.com>
2021-05-21 12:24:21 -0700

    Add env files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,17 @@
+import {config as dotenvSafeConfig} from "dotenv-safe";
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+dotenvSafeConfig({
+    path: isDevelopment ? "dev.env" : ".env",
+    example: ".env.example"
+});
+
 import { STATIC_PATH } from "./constants/Paths";
 import { baseRouter } from "./routers/base.router";
 import { Server } from "./server/Server";
 
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT || "3000");
 
 const server = new Server(STATIC_PATH);
 
