a7e1065690ffc32e546d5fd502f1b9c600cd62c0
Nick Musey <nmusey@gmail.com>
2021-05-20 22:26:06 -0700

    Improve webpack
    - Add support for more filetypes
    - Add favicon to site
    - add clean-webpack-plugin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
     entry: path.resolve(__dirname, "src", "index.tsx"),
@@ -11,27 +14,38 @@ module.exports = {
     resolve: {
         extensions: [".ts", ".tsx", ".js"],
     },
+    devtool: isDevelopment ? "eval" : false,
     module: {
         rules: [
-        {
-            test: /\.(ts|tsx)$/,
-            loader: "ts-loader",
-            exclude: /node_modules/
-        },
-        {
-            enforce: "pre",
-            test: /\.js$/,
-            loader: "source-map-loader",
-        },
-        {
-            test: /\.css$/,
-            loader: "css-loader",
-        },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
+            },
+            {
+                test: /\.less$/,
+                use: ["style-loader", "css-loader", "less-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(jpeg|png|gif|svg)$/,
+                type: 'asset/resource'
+            }
         ],
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, ".", "assets", "index.html"),
-        })
+            template: path.resolve(__dirname, "assets", "index.html"),
+            favicon: path.resolve(__dirname, "assets", "favicon", "favicon.ico")
+        }),
+        new CleanWebpackPlugin()
     ],
 };
