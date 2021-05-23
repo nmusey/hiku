634ed32fbadc2346baee3ec8d2e3226f9f0b35fa
Nick Musey <nmusey@gmail.com>
2021-05-22 19:49:39 -0700

    Create pages subdirectory

--- client/src/pages/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { UnauthenticatedLayout } from "../components/layouts/UnauthenticatedLayout";
-import { Card } from "antd";
-
-export const LoginPage = (): JSX.Element => (
-    <UnauthenticatedLayout>
-        <Card hoverable className="fill" title="Login page">This is a filler card</Card>
-    </UnauthenticatedLayout>
-);
