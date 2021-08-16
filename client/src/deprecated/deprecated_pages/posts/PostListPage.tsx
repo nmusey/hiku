850eb6b29151d24424ef7709981dd556459f994d
Nick Musey <nmusey@gmail.com>
2021-08-16 07:04:06 -0700

    Remove deprecated files

--- client/src/deprecated/deprecated_pages/posts/PostListPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { AuthenticatedLayout } from "../../deprecated_components/layouts/AuthenticatedLayout";
-import { PostList } from "../../deprecated_components/post/PostList";
-
-export const PostListPage = (): JSX.Element => (
-    <AuthenticatedLayout>
-        <PostList />
-    </AuthenticatedLayout>
-);
